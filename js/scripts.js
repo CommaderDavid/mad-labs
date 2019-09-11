31ad0bc2a164069d0acde55ff3fcfa49181c3ec5
David Boedigheimer <stealth259@gmail.com>
2019-09-11 10:03:22 -0700

    Add variables to inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
 
     $(".person1").append(person1Input);
     $(".person2").append(person2Input);
