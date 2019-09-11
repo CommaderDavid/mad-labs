0580640ed1493e37020342d93282ce88829b9e8b
David Boedigheimer <stealth259@gmail.com>
2019-09-11 11:34:53 -0700

    Add extra and addon to the story.

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,17 @@ $(document).ready(function() {
     var person2Input = $("input#person2").val();
     var animalInput = $("input#animal").val();
     var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
+    var verb1Input = $("input#verb1").val();
     var nounInput = $("input#noun").val();
+    var verb2Input = $("input#verb2").val();
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
+    $(".verb1").text(verb1Input);
     $(".noun").text(nounInput);
+    $(".verb2").text(verb2Input)
 
     $("#story").show();
 
